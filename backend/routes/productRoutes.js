f5fc97c04fac094c5da6578f52590086ec46f294
Eric <rckplgt@gmail.com>
2024-03-23 22:16:42 +0300

    initializing redux

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,28 +1,11 @@
 import express from 'express'
-import asyncHandler from '../middleware/asyncHandler.js'
-//import products from '../data/products.js'
-import Product from '../models/productModel.js'
+//import router from express.Router()
+import { getProducts, getProductById } from '../controllers/productController.js'
 
 const router = express.Router()
 
-router.get('/', asyncHandler(async (req,res)=>{
-    const products = await Product.find({})
-    res.json(products)
-}))
+router.route('/').get(getProducts)
 
-router.get('/:id', asyncHandler(async (req,res)=>{
-    const product = await Product.findById(req.params.id)
-
-    if (product){
-        return res.json(product)
-    }else{
-        res.status(404)
-        throw new Error('product not found')
-    }
-
-    res.status(404).json({message: 'Resource not found'})
-
-    res.json(product)
-}))
+router.route('/:id').get(getProductById)
 
 export default router
